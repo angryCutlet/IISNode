49d8c87ae3706da969281126b148f03c884ec62f
Tadas Ruginis <angryCutlet@gmail.com>
2015-01-14 11:51:13 -0500

    stuck at modified

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 var express = require("express")
 var app = express()
 
-app.get('/', function(req, res){
-    res.send('Hello world with node and express')
-})
+app.use(express.static(__dirname+'/public'))
 
 var server = app.listen(3000, function(){
     var host = server.address().address
